417d1f7c20f756b338c39814bb532414ded763b4
gerardo-orozco <orozcomgera@gmail.com>
2014-11-08 17:51:28 -0500

    Avoid killing the terminal before installing brews and casks. Duh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-# First install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git
-# Download the dotfiles!
+# First install Homebrew and Git
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2> /dev/null
+brew install git 2> /dev/null
+
+echo -n "Download the dotfiles..."
 cd; curl -#L https://github.com/gerardo-orozco/dotfiles/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,install.sh,LICENSE-MIT.txt}
+echo "[DONE]"
+
+echo -n "Installing RVM"
+\curl -sSL https://get.rvm.io | bash -s stable --ruby 2> /dev/null
+echo "[DONE]"
+
 source ~/.bash_profile
-# Install formulae and RVM (http://rvm.io/rvm/install)
-./.osx && ./brew.sh && \curl -sSL https://get.rvm.io | bash -s stable --ruby
+./.osx && ./brew.sh
