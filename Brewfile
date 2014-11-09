f9be4fe606162b480ecadc41549ef5ae18622e6f
gerardo-orozco <orozcomgera@gmail.com>
2014-11-08 20:42:26 -0500

    Split brews and casks into separate files

---
+++ Brewfile
@@ -0,0 +1,28 @@
+moreutils
+findutils
+gnu-sed --default-names
+bash
+bash-completion
+wget --with-iri
+ringojs
+narwhal
+vim --override-system-vi
+homebrew/dupes/grep
+homebrew/dupes/screen
+homebrew/php/php55 --with-gmp
+ack
+git
+imagemagick --with-webp
+lynx
+p7zip
+pigz
+pv
+rename
+tree
+webkit2png
+python
+node
+homebrew/versions/lua52
+libjpeg
+libpng
+hub
--- brew.sh
+++ brew.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 echo "Install command-line tools using Homebrew."
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -17,106 +17,50 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-echo "Installing packages..."
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# @TODO: review these later
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
-# Install other useful binaries.
-brew install ack
-brew install git
-brew install imagemagick --with-webp
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install tree
-brew install webkit2png
-
-brew install python
-brew install node
-brew install homebrew/versions/lua52
-
-brew install libjpeg
-brew install libpng
-
-brew install hub
-
 ##############################################################################
-# Casks
+# Formulas
 ###############################################################################
-# First install Caskroom
+echo "------------------------------------------------------------------------"
+echo "Installing formulas"
+echo "------------------------------------------------------------------------"
+formulae=$(<Brewfile)
+for formula in $formulae; do
+    formula_name=$(echo $formula | cut -d' ' -f1)
+    if [ -z "${formula_name}" ]; then
+        if brew list -1 | grep -q "^${formula_name}\$"; then
+            echo "+ '$formula_name' already installed"
+        else
+            brew install $formula
+        fi
+    fi
+done
+
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
-brew cask install adobe-reader
-brew cask install android-file-transfer
-brew cask install cdock
-brew cask install dropbox
-brew cask install evernote
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install google-hangouts
-brew cask install heroku-toolbelt
-brew cask install kivy
-brew cask install sizeup
-brew cask install skitch
-brew cask install skype
-brew cask install spotify
-brew cask install transmission
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install vlc
-brew cask install xquartz
-brew cask install iterm2
+###############################################################################
+# Casks
+###############################################################################
+echo "------------------------------------------------------------------------"
+echo "Installing Casks"
+echo "------------------------------------------------------------------------"
+formulae=$(<Caskfile)
+for formula in $formulae; do
+    formula_name=$(echo $formula | cut -d' ' -f1)
+    if [ -z "${formula_name}" ]; then
+        if brew cask list -1 | grep -q "^${formula_name}\$"; then
+            echo "+ '$formula_name' already installed"
+        else
+            brew cask install $formula
+        fi
+    fi
+done
 
 # Remove outdated versions from the cellar.
 brew cleanup
