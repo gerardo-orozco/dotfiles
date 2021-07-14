e5612f1f31229eef75b1660fa5cb1fc779a1554c
gerardo-orozco <orozcomgera@gmail.com>
2021-07-13 23:18:54 -0500

    kickstart: rm trap

--- kickstart/kickstart.sh
+++ kickstart/kickstart.sh
@@ -1,8 +1,4 @@
-function goback() {
-  popd > /dev/null
-}
-pushd ${HOME} > /dev/null
-trap goback ERR EXIT
+cd ${HOME}
 curl -#L https://github.com/gerardo-orozco/dotfiles/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,install.sh,LICENSE-MIT.txt} 1> /dev/null
 [[ "$?" != "0" ]] && exit 1
 for f_name in 'osx_defaults.sh brew.sh sublime.sh rvm.sh'; do
